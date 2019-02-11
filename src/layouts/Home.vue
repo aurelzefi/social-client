efef3bb9a8ddb5fd579d49189c051c490270be61
Aurel Zefi <aurelzefi1994@gmail.com>
2019-02-11 19:56:59 +0100

    added layouts

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  $END$
+  <layout>
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-md-3 col-lg-2 mb-4">
+          <profile-card/>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-4">
+          <slot/>
+        </div>
+
+        <div class="col-md-3 col-lg-2">
+          <suggestions/>
+        </div>
+      </div>
+    </div>
+  </layout>
 </template>
 
 <script>
-  export default {
-    name: 'Home'
-  };
+import Layout from './Main.vue';
+import ProfileCard from '../components/ProfileCard.vue';
+import Suggestions from '../components/Suggestions.vue';
+
+export default {
+  components: { Layout, ProfileCard, Suggestions },
+};
 </script>
-
-<style scoped>
-
-</style>
--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,15 +1,155 @@
 <template>
   <div>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <router-link class="navbar-brand d-none d-sm-block" to="/">MySocial</router-link>
 
+        <form class="form-inline mr-auto" v-if="user" @submit.prevent="search">
+          <div class="input-group">
+            <input id="search" type="text" class="form-control" placeholder="Search"
+                   aria-label="Search" autocomplete="off" v-model="query"
+                   @click="showSearch = true" @keyup="getResults">
+            <div class="input-group-append">
+              <button type="submit" class="btn btn-outline-primary">Search</button>
+            </div>
+          </div>
+
+          <div class="position-absolute" style="top: 50px;">
+            <div class="list-group" v-if="showSearch">
+              <router-link class="list-group-item list-group-item-action"
+                           :to="`/users/${user.id}/posts`" v-for="user in users" :key="user.id">
+                <img :src="avatar(user)" class="rounded-circle mr-2" width="30" height="30"
+                     :alt="user.name">
+                {{ user.name }}
+              </router-link>
+            </div>
+          </div>
+        </form>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto" v-if="! user">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/register">Register</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+          </ul>
+
+          <ul class="navbar-nav ml-auto" v-if="user">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/home">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/messages">
+                Messages ({{ unreadConversationsCount }})
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/notifications">
+                Notifications ({{ user.unread_notifications_count }})
+              </router-link>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle action-link" id="navbarDropdown" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="rounded-circle" :src="avatar(user)" width="20" heigth="20">
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <router-link class="dropdown-item" to="/settings/account">Settings</router-link>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item action-link" @click="logout">Logout</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <main>
+      <slot/>
+    </main>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState, mapGetters, mapMutations } from 'vuex';
 
+export default {
+  data() {
+    return {
+      query: '',
+      users: [],
+      limit: 10,
+      showSearch: false,
+    };
+  },
+
+  computed: {
+    ...mapState([
+      'user',
+    ]),
+
+    ...mapGetters([
+      'unreadConversationsCount',
+    ]),
+  },
+
+  mounted() {
+    this.query = this.$route.query.query ? this.$route.query.query : '';
+
+    $('body').click((e) => {
+      if (e.target.id !== 'search') {
+        this.showSearch = false;
+      }
+    });
+  },
+
+  methods: {
+    ...mapMutations([
+      'unsetUser',
+    ]),
+
+    logout() {
+      axios.post('/logout').then(() => {
+        echo.leave(`user.${this.user.id}`);
+
+        this.unsetUser();
+
+        localStorage.removeItem('token');
+
+        delete axios.defaults.headers.Authorization;
+
+        this.$router.push('/');
+      });
+    },
+
+    search() {
+      if (this.query.length > 0) {
+        this.$router.push(`/search?query=${this.query}`);
+      }
+    },
+
+    getResults(e) {
+      if (this.query.length === 0 || e.key === 'Enter') {
+        this.users = [];
+
+        return;
+      }
+
+      axios.get(`/search/users?limit=${this.limit}&query=${this.query}`)
+        .then((response) => {
+          this.showSearch = true;
+          this.users = response.data;
+        });
+    },
+  },
 };
 </script>
-
-<style scoped>
-
-</style>
